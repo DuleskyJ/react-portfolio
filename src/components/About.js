43b3dc78a3f6ab76c0152204ce204b287f5a0271
Laptop <jdkyaz@gmail.com>
2024-07-02 06:46:15 -0700

    importing styles.css and Added more information about me

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ function About() {
   return (
     <section>
       <h2>About Me</h2>
-      <img src="path-to-your-photo.jpg" alt="Your Name" />
+      <img src="/assets/images/me.png" alt="A picture of Jaden how handsome" />
       <p>HEY! It's me Jaden D and welcome to my portfolio page! Here I'll display all my projects I complete as I further my coding knowledge as a current full-stack student. It's definetely a switch up from managing people serving coffee!</p>
     </section>
   );
