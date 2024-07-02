039b0d8a9242785bf48cda5a9fa9483fdebfceaa
Laptop <jdkyaz@gmail.com>
2024-07-02 09:26:55 -0700

    Fix ESLint issue with redundant alt attribute in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function About() {
   return (
-    <section>
+    <section className="about">
       <h2>About Me</h2>
-      <img src="/assets/images/me.png" alt="A picture of Jaden how handsome" />
-      <p>HEY! It's me Jaden D and welcome to my portfolio page! Here I'll display all my projects I complete as I further my coding knowledge as a current full-stack student. It's definetely a switch up from managing people serving coffee!</p>
+      <img src="/assets/images/me.png" alt="Jaden D" />
+      <p>
+        HEY! It's me Jaden D and welcome to my portfolio page! Here I'll display all my projects I complete as I further my coding knowledge as a current full-stack student. It's definitely a switch up from managing people serving coffee!
+      </p>
     </section>
   );
 }
