30f204a4376d11ebcb02b7d870575031c4b804b2
Laptop <jdkyaz@gmail.com>
2024-07-02 06:50:32 -0700

    oops imported a nonexistent file im tired

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Contact from './components/Contact';
 import Resume from './components/Resume';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import './styles.css';
+import './App.css';
 
 function App() {
   return (
